974b4f1174ff0b96e8886efcc333f8598474f136
Jeremy Ellis <keyfreemusic@gmail.com>
2019-08-10 17:12:41 -0700

    Update app.js

--- app.js
+++ app.js
@@ -6,17 +6,50 @@ var express = require('express');
 
 var app = express();
 
+///////////////////////////////
+// Fetch your HELLOWORLD Message
+///////////////////////////////
+
+const iotaLibrary = require('@iota/core')
+
+const iota = iotaLibrary.composeAPI({
+    provider: 'https://iotanode.us:443'
+
+})   //  provider: 'https://nodes.devnet.thetangle.org:443'
+
+const myReceiveAddress =
+  'RJKYCCRHVRZCRZZ9AEAGCSRGUBNIHDTPBWVKQRALYHOUGTWSYWFFKMNVAWDASDLFEVDBNHRNQKLTQYZZACHJHONACW'   // temp1 recieve
+
+
+
+
+
+iota
+  .findTransactionObjects({ addresses: [myReceiveAddress] })
+  .then(response => {
+   // console.log(response)
+       global.myResponse22 = '<h2>2.2-fetch-hello.js</h2>' + '<pre id="myPre01">'+JSON.stringify(response, null, 3)+'</pre>' + '<hr>';  // hopefully this is global
+
+  })
+  .catch(err => {
+    console.error(err)
+  })
+
+
+
+
+
+
+
 
 
 app.get('/', function(req, res) {
    let myCombined = `
-       Cool this works <br> and this as well
-   
-   
-   
-   `
+       <h3 align=center>IOT enocuramgemnt </h3>
+       `+global.myResponse22+`
 
 
+   `    // end long string
    res.send(myCombined);
 
 });
