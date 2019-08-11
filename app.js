a49f92a1f45b2683b78aa413c68d2a148cafc635
Jeremy Ellis <keyfreemusic@gmail.com>
2019-08-11 13:04:26 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-// destroy the online build by the next comment. remove to work
 
 var express = require('express');
 var app = express();
 
 
 const mySeed =
-  'DONOTSTOREYOURSEEDONAPUBLICGITHUBSITEASANYONECANSTEALALLYOUR9IOTATOKENSKEEPITSAFE'
+  'DONOTSTOREYOURSEEDONAPUBLICGITHUBSITEASANYONECANSTEALALLYOUR9IOTATOKENSKEEPITSAFE'   //temp1 seed
 
-let myReceiveAddress = '' // This will be immediately generated
+global.myReceiveAddress = 'QNLY9LSWBFKMXTJSYJQOJXDJ99HMXHLJYLOLCV9ONOUUZQZAXIURIKGZ9GJ9UBPKUAUVTWZDGPZGST9DDBCKKMR9PD' // This will be immediately generated
 
 
 const iotaLibrary = require('@iota/core')
@@ -19,14 +18,34 @@ const iota = iotaLibrary.composeAPI({
 
 
 
-function myGenerateAddressFromSeed(myPassedSeed){
+async function myGenerateAddressFromSeed(myPassedSeed){
+
+	var options = {
+		checksum: true,
+		security: 2,
+        index : 0,
+        total : 7
+       // total : 1   //,
+		//index: 0     // if 0 it should always return the latest address
+        // wierd if index is less than the present unused it generates the unused. If index is
+        // more than the unused it generates the index.
+	}
+
 
 iota
-  .getNewAddress(myPassedSeed)
+  .getNewAddress(myPassedSeed, options)
   .then(myGenAddress => {
      console.log('Your address is: ' + myGenAddress )
      global.myResponse1 = '<h2>Send a small amount of tokens to: </h2>' + '<pre id="myPre01">'+JSON.stringify(myGenAddress, null, 3)+'</pre>' + '<hr>';  // hopefully this is global
-     myReceiveAddress = myGenAddress
+     if (global.myReceiveAddress == myGenAddress){
+        console.log('No change your recieve address is still: ' + myGenAddress )
+        global.myResponse1 += 'No change your recieve address is still: ' + myGenAddress
+     } else {
+        console.log('Cool a change has occured and your new address is: ' + myGenAddress )
+        global.myResponse1 += 'Cool a change has occured and your new address is: ' + myGenAddress
+     }
+     global.myReceiveAddress = myGenAddress
+     myCheckTransactionUsingAddress(global.myReceiveAddress)
   })
   .catch(err => {
     console.log(err)
@@ -36,9 +55,9 @@ iota
 }
 
 
-function myCheckTransactionUsingAddress(){
+async function myCheckTransactionUsingAddress(myRAddress){
 iota
-  .findTransactionObjects({ addresses: [myReceiveAddress] })
+  .findTransactionObjects({ addresses: [myRAddress] })
   .then(response => {
        console.log(response)
        global.myResponse2 = '<h2>2.2-fetch-hello.js</h2>' + '<pre id="myPre01">'+JSON.stringify(response, null, 3)+'</pre>' + '<hr>';  // hopefully this is global
@@ -53,9 +72,13 @@ iota
 
 /////////////////////////////////// run the main app //////////////////////////////////
 
-myGenerateAddressFromSeed(mySeed)
-//myCheckTransactionUsingAddress()
+//myGenerateAddressFromSeed(mySeed)      // called using an interval
+//myCheckTransactionUsingAddress(global.myReceiveAddress)   // called inside myGenerateAddressFromSeed
 
+myGenerateAddressFromSeed(mySeed)
+let myTimer = setInterval(function(){
+         myGenerateAddressFromSeed(mySeed)
+     }, 100000);   // end setInterval
 
 
 
